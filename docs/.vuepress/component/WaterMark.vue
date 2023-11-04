c16a054a28f43bf34deb0a899d0f465345066355
夏之一周 <1602453034@qq.com>
2023-11-05 00:24:29 +0800

    fix:window is not defined

--- docs/.vuepress/component/WaterMark.vue
+++ docs/.vuepress/component/WaterMark.vue
@@ -1,10 +1,10 @@
 <script setup>
 import { nextTick } from 'vue'
 import watermark from "../utils/watermark";
-nextTick(()=>{
-  document.querySelector(".page") &&  watermark.set('夏之一周 wudetian.top (吾的天)', window.document.querySelector('.page'))
+nextTick(() => {
+  if (typeof window !== 'undefined') {
+    window.document.querySelector(".page") && watermark.set('夏之一周 wudetian.top (吾的天)', window.document.querySelector('.page'))
+  }
 })
 </script>
-<template>
-
-</template>
+<template></template>
