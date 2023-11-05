290a313e6cb8976e730861ef585584f127d7e088
夏之一周 <1602453034@qq.com>
2023-11-05 23:05:11 +0800

    fix:DOMException: Failed to execute querySelectorAll on Document: head > /[0=f] is not a valid selector.

--- docs/.vuepress/component/WaterMark.vue
+++ docs/.vuepress/component/WaterMark.vue
@@ -3,7 +3,7 @@ import { nextTick } from 'vue'
 import watermark from "../utils/watermark";
 nextTick(() => {
   if (typeof window !== 'undefined') {
-    window.document.querySelector(".page") && watermark.set('夏之一周 wudetian.top (吾的天)', window.document.querySelector('.page'))
+    window.document.querySelector("body .page") && watermark.set('夏之一周 wudetian.top (吾的天)', window.document.querySelector("body .page"))
   }
 })
 </script>
